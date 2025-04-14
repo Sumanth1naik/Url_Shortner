628e1e6cd9ee3ed2e922aa8172d2a276adf08874
Sumanth Naik <sumanthmuttu524@gmail.com>
2025-04-14 16:34:52 +0530

    updated the main class to handle the logic calls from the main function

--- MainApp.java
+++ MainApp.java
@@ -1,9 +1,23 @@
-public class MainApp {
-    public static void main(String[] args) {
-        UrlRepository repo = new UrlRepository();
-        repo.fetchAllUrls();  // Fetch & print all URLs
+import java.util.Scanner;
 
-        // Close DB connection before exiting
-        Runtime.getRuntime().addShutdownHook(new Thread(DatabaseConnection::closeConnection));
+public class MainApp {
+
+    public static String urlShortner(String longUrl){
+        url_Shortner classObj = new url_Shortner();
+        String url = longUrl;
+        System.out.print("Entered  URL for shorten: "+url);
+        // Call the Url_Shrinker method and print the result
+        String shortUrl = classObj.Url_Shrinker(url);
+        System.out.println("Shortened URL: " + shortUrl);
+        return shortUrl;
+    }
+
+    public static String urlExpander(String url){
+        url_Shortner classObj = new url_Shortner();
+        // Prompt the user for a short URL
+        System.out.print("Entered short URL for expand: ");
+        // Call the Url_Expander method and print the result
+        String longUrl = classObj.Url_Expander(url);
+        return longUrl;
     }
 }
